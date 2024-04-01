97b01c57f4819a04f37f87eb84ff47d5cb7a04bd
Hp <Hp@DESKTOP-2KUVAQ5>
2024-04-02 00:58:01 +0300

    Add project files.

---
+++ WebApiWork/Controllers/AdminController.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebApiWork.Models;
+
+namespace WebApiWork.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AdminController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            PanelContext context = new PanelContext();
+            List<AdminUser> adminUser = context.AdminUsers.ToList();
+            return Ok(adminUser);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetById(int id)
+        {
+            PanelContext context = new PanelContext();
+            AdminUser adminUser = context.AdminUsers.FirstOrDefault(u => u.Id == id);
+
+            if (adminUser == null)
+            {
+                return NotFound("Kullanıcı bulunamadı!");
+            }
+
+            return Ok(adminUser);
+        }
+
+        [HttpPost]
+        public IActionResult Create(AdminUser user)
+        {
+            PanelContext context = new PanelContext();
+            context.AdminUsers.Add(user);
+            context.SaveChanges();
+
+            return Ok("Kayıt başarılı");
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            PanelContext context = new PanelContext();
+            AdminUser adminUser = context.AdminUsers.FirstOrDefault(x => x.Id == id);
+
+            if (adminUser == null)
+            {
+                return NotFound("Kullanıcı bulunamadı!");
+
+            }
+            context.AdminUsers.Remove(adminUser);
+            context.SaveChanges();
+            return Ok("Silme işlemi başarılı");
+        }
+
+        [HttpPut]
+        public IActionResult Update(AdminUser user)
+        {
+            PanelContext context = new PanelContext();
+            AdminUser adminUser = context.AdminUsers.FirstOrDefault(x => x.Id == user.Id);
+
+            adminUser.UserName = user.UserName;
+            adminUser.SurName = user.SurName;
+            adminUser.AddDate = user.AddDate;
+            adminUser.IsDeleted = user.IsDeleted;
+            adminUser.Email = user.Email;
+            context.SaveChanges();
+
+            return Ok("Güncelleme işlemi başarılı");
+        }
+    }
+}
---
+++ WebApiWork/Models/PanelContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WebApiWork.Models
+{
+    public class PanelContext :DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Data Source=DESKTOP-2KUVAQ5\SQLEXPRESS;Initial Catalog=WebApiPanel;Integrated Security=True;TrustServerCertificate=True;");
+        }
+
+        public DbSet<AdminUser> AdminUsers { get; set; }
+    }
+}
